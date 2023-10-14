fd7b5788d4a95da53504c6eaa9856e7f10f36d29
iLevsPHP_ <202215210015@ifba.edu.br>
2023-10-13 21:40:29 -0300

    O inicio

---
+++ aula006/app/view/inicial.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visitas - 2º Vespertino</title>
+</head>
+<body>
+    <h1>Visitas - 2º Vespertino</h1>
+    <form method="post" action="salvar">
+        <label>
+            Nome:
+            <input type="text" name="nome">
+        </label>
+        <button>Confirmar Visita</button>
+    </form>
+</body>
+</html>
