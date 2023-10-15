9f36cbe09af7b7ff17e1fd067b5200f548b13534
PauloHenryLimaBomfim <zzHenryx@gmail.com>
2023-10-15 11:46:07 -0300

    Vasco > VarMengo

--- aula006/app/view/inicial.php
+++ aula006/app/view/inicial.php
@@ -4,55 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link href="../../estilo.css" rel="stylesheet">
+    
+
     <link href='https://fonts.googleapis.com/css?family=Irish Grover' rel='stylesheet'>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=League+Gothic&display=swap" rel="stylesheet">
 
     <style type="text/css">
-        body{
-            background-color: black;
-        }
-        header{
-            display: flex;
-            justify-content: center;
-        }
-        header h1{
-            font-family: 'Irish Grover';
-            font-size: 50px;
-            color: white;
-            margin-top: 50px;
-            margin-bottom: 50px;
-        }
-        header b{
-            color: #F0C82F;
-        }
-        label{
-            font-family: 'League Gothic';
-            color: white;
-            font-size: 40px;
-            
-        }
-        form input{
-            background-color: black;
-            padding: 20px;
-            width: 65%;
-            height: 10px;
-            border: yellow 1px solid;
-            border-radius: 10px;
-            outline: unset;
-            margin-bottom: 10px;
-        }
+       
     </style>
     <title>Visitas - 2º Vespertino</title>
 </head>
 <body>
-    <header>
-        <h1> REGISTRE <b>SUA VISITA</b> EM NOSSA SALA </h1>
-    </header>
     <main>
+        <h1> REGISTRE <b>SUA VISITA</b> EM NOSSA SALA </h1>
         <form method="post" action="salvar">
             <label>
                 NOME:
@@ -61,5 +28,9 @@
             <button>Confirmar Visita</button>
         </form>
     </main>
+    <footer>
+        <h2> ACESSE O LIVRO DE VISITAS</h2>
+        <h3> <b>DOSCENTE:</b> THIAGO MIRANDA</h3>
+    </footer>
 </body>
 </html>
