bb3fce85a1bc9e77f1c46c054c6ba11dc9a4537f
Guilherme Gama <ggamaa780@gmail.com>
2023-10-15 14:41:48 -0300

    mostra

--- aula006/app/view/mostra.php
+++ aula006/app/view/mostra.php
@@ -4,20 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../estilo2.css" rel="stylesheet">
     <title>Visitas - 2º Vespertino</title>
 </head>
-<body>
-    <h1>Visitas - 2º Vespertino</h1>
-    <ul>
-        <li>03/10/22023 - 09:45 - João Da Silva</li>
-        <li>03/10/22023 - 09:45 - João Da Silva</li>
-        <li>03/10/22023 - 09:45 - João Da Silva</li>
-        <li>03/10/22023 - 09:45 - João Da Silva</li>
-        <li>03/10/22023 - 09:45 - João Da Silva</li>
-        <li>03/10/22023 - 09:45 - João Da Silva</li>
-        <li>03/10/22023 - 09:45 - João Da Silva</li>
-        <li>03/10/22023 - 09:45 - João Da Silva</li>
-    </ul>
+<body id="mostra">
+
+    <main>
+        <div class="part">
+            <h1 class="nome-lista">Lista de Visitantes - 2º Vespertino 2° ANO VESP INF</h1>
+            <div class="mostrar-lista">
+                <ul>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                </ul>
+            </div>
+        </div>
+        <div class="part2">
+            <div class="cena-de-crime"><img src="../../imagens/cenaCrime 1.png"></div>
+        </div>
+    </main>
+
+    <footer>
+        <div class="infor"></div>
+        <div class="img"></div>
+    </footer>
 
 </body>
 </html>
