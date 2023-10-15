73ff0b038bd9370eae9792257542432ad0997dc4
Guilherme Gama <ggamaa780@gmail.com>
2023-10-15 15:11:13 -0300

    atualização

--- aula006/app/view/mostra.php
+++ aula006/app/view/mostra.php
@@ -5,15 +5,60 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../../estilo2.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Irish Grover' rel='stylesheet'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <title>Visitas - 2º Vespertino</title>
 </head>
 <body id="mostra">
 
     <main>
         <div class="part">
-            <h1 class="nome-lista">Lista de Visitantes - 2º Vespertino 2° ANO VESP INF</h1>
+            <div class="titulo-lista">
+                <h1>Lista de Visitantes</h1>
+                <h2>2° ANO VESP INF</h2>
+            </div>
             <div class="mostrar-lista">
                 <ul>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
+                    <li>03/10/22023 - 09:45 - João Da Silva</li>
                     <li>03/10/22023 - 09:45 - João Da Silva</li>
                     <li>03/10/22023 - 09:45 - João Da Silva</li>
                     <li>03/10/22023 - 09:45 - João Da Silva</li>
